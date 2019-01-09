60eee2f2b56774339456f49377d10b9b371f8c65
unknown <jdavid.stevens@gmail.com>
2019-01-09 16:39:12 -0700

    Centered

--- stripe-test-project/src/App.js
+++ stripe-test-project/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import axios from 'axios';
+import './App.css'
 
 class App extends Component {
 constructor(){
